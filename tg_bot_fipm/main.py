db60d91fe0fcf6cfa1d69531518b3295d3344177
Fwuff <FwuffFox@gmail.com>
2024-09-12 09:50:01 +0300

    closer to finish

--- tg_bot_fipm/main.py
+++ tg_bot_fipm/main.py
@@ -5,7 +5,7 @@ import sys
 from os import getenv
 from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher, html
+from aiogram import F, Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
@@ -27,8 +27,9 @@ from game import (
     current_players,
 )
 import result_writer
+import texts
 
-load_dotenv()
+load_dotenv(override=True)
 
 env_token = getenv("BOT_TOKEN")
 if not env_token:
@@ -47,11 +48,35 @@ async def command_start_handler(message: Message, state: FSMContext) -> None:
     if message.chat.id in current_players or await state.get_state() is not None:
         await message.answer("Вы уже начали или прошли игру!")
         return
-    
+
     await state.set_state(GameState.name_collection)
 
-    await message.answer("Приветствие")
+    await message.answer(texts.GREETING)
     await message.answer("Введите группу которую вы представляете (Например ПИ-2401):")
+
+
+@dp.message(Command("back"), GameState.task_reply)
+@dp.message(F.text.casefold() == "back", GameState.task_reply)
+async def go_back_to_selection(message: Message, state: FSMContext):
+    await state.set_state(GameState.task_selection)
+
+    await message.answer("Возвращение к выбору вопроса...")
+    await display_tasks(message, state)
+
+
+async def premature_end_command(message: Message, state: FSMContext):
+    player_data: PlayerData = (await state.get_data()).get("player_data", None)
+
+    if player_data.name == "":
+        await message.answer("Нельзя закончить игру")
+
+    for i, task in enumerate(tasks):
+        if i in player_data.tasks:
+            continue
+        player_data.tasks[i] = 0
+
+    await state.update_data(player_data=player_data)
+    await process_end_game(message, state)
 
 
 @dp.message(GameState.name_collection)
@@ -65,7 +90,7 @@ async def collect_name_and_start_game(message: Message, state: FSMContext) -> No
     if name in groups_that_started:
         await message.answer("Группа с таким именем уже начала игру.")
         return
-    
+
     handle = message.from_user.username if message.from_user is not None else ""
     handle = handle if handle is not None else ""
     player_data = PlayerData(name, handle, id)
@@ -87,7 +112,7 @@ async def display_tasks(message: Message, state: FSMContext) -> None:
         if i in player_data.tasks:
             continue
 
-        if len(cur_row) == 2:
+        if len(cur_row) == 1:
             buttons.append([])
             cur_row = buttons[-1]
 
@@ -98,7 +123,7 @@ async def display_tasks(message: Message, state: FSMContext) -> None:
         return
 
     await message.answer(
-        text="Выберите название станции на которой сейчас находитесь:",
+        text="Выберите название уровня, на котором вы находитесь:",
         reply_markup=InlineKeyboardMarkup(inline_keyboard=buttons),
     )
     await state.set_state(GameState.task_selection)
@@ -117,7 +142,9 @@ async def task_selection_handler(query: CallbackQuery, state: FSMContext) -> Non
     await state.update_data(player_data=player_data)
     await state.set_state(GameState.task_reply)
 
-    await query.message.answer("Введите полученный ответ")  # type: ignore
+    await query.message.answer(  # type: ignore
+        f"Введите ответ на задание '{tasks[cur_task]}'\nЛибо нажмите \\back чтобы вернутся к выбору вопроса:"
+    )
 
 
 @dp.message(GameState.task_reply)
@@ -125,12 +152,14 @@ async def process_task_reply(message: Message, state: FSMContext) -> None:
     player_data: PlayerData = (await state.get_data()).get("player_data", None)
 
     if player_data.cur_task in player_data.tasks:
-        await message.answer("(ОШИБКА) Вы уже ответили на это задание!")
+        await message.answer(f"{html.bold("ОШИБКА")} Вы уже ответили на это задание!")
         await display_tasks(message, state)
         return
 
     if not message.text:
-        await message.answer("Введите ответ полученный у куратора!")
+        await message.answer(
+            f"Введите ответ на задание '{tasks[player_data.cur_task]}':"
+        )
         return
 
     points = get_points_from_answer(message.text)
@@ -139,11 +168,7 @@ async def process_task_reply(message: Message, state: FSMContext) -> None:
 
     await state.set_state(GameState.task_selection)
 
-    await message.answer(
-        f"{html.bold("УБРАТЬ ПОСЛЕ ТЕСТИРОВАНИЯ")}\n"
-        + f"Вы получили {points} баллов за задание {tasks[player_data.cur_task]}\n"
-        + f"Текущие результаты: {player_data.tasks}"
-    )
+    await message.answer("Ответ принят.")
 
     await display_tasks(message, state)
 
@@ -151,33 +176,13 @@ async def process_task_reply(message: Message, state: FSMContext) -> None:
 async def process_end_game(message: Message, state: FSMContext) -> None:
     player_data: PlayerData = (await state.get_data()).get("player_data", None)
     player_data.fixate_end_time()
-    total_points = sum(player_data.tasks.values())
 
-    await message.answer(
-        f"Игра окончена! Ваш результат: {total_points} баллов."
-        + f"Полученные баллы за задания: {player_data.tasks}"
-    )
+    await message.answer(texts.GAME_END)
 
     current_players.remove(player_data.id)
     await state.clear()
 
     result_writer.write(player_data)
-
-
-@dp.message(Command("end"))
-async def premature_end_command(message: Message, state: FSMContext):
-    player_data: PlayerData = (await state.get_data()).get("player_data", None)
-
-    if player_data.name == "":
-        await message.answer("Нельзя закончить игру")
-
-    for i, task in enumerate(tasks):
-        if i in player_data.tasks:
-            continue
-        player_data.tasks[i] = 0
-
-    await state.update_data(player_data=player_data)
-    await process_end_game(message, state)
 
 
 async def times_up():
@@ -201,8 +206,11 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    file = "bot.log"
-    with open(file, mode="w") as f:
-        print(f"Запуск бота. Файл с логами: {os.path.realpath(f.name)}")
-        logging.basicConfig(level=logging.INFO, stream=f)
-        asyncio.run(main())
+    file = getenv("LOG_FILE")
+    if file is None:
+        logging.basicConfig(level=logging.INFO)
+    else:
+        with open(file, mode="w") as f:
+            print(f"Запуск бота. Файл с логами: {os.path.realpath(f.name)}")
+            logging.basicConfig(level=logging.INFO, stream=f)
+    asyncio.run(main())
